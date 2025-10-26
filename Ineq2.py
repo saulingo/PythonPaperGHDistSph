ff04d31910e4d425c160d89fe6bdfc6000be0194
saulingo <61324317+saulingo@users.noreply.github.com>
2025-10-26 00:16:37 -0400

    Update Ineq2.py

--- Ineq2.py
+++ Ineq2.py
@@ -6,7 +6,7 @@ C  = lambda κ: -np.sqrt(1 - 2*np.cos(eta3 - κ) + 16*np.cos(eta3 - κ)**2) / np
 L  = lambda κ: np.arccos(np.cos(2)*(np.cos(κ)-1)/2 + (1+np.cos(κ))/2)
 U3 = lambda κ: np.arccos((-4/np.sqrt(15)) * np.sqrt(C(κ)**2 + np.cos(np.pi - eta3 + κ)**2 - 0.5*C(κ)*np.cos(np.pi - eta3 + κ)))
 
-step = 1e-7
+step = 1e-6
 κ, fmax = 0.3, -np.inf
 
 while κ <= 0.7:
